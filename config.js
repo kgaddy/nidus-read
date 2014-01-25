496a55ce08b104656dd27d631d1bc1c2abc06845
Kevin Gaddy <kevin.gaddy@gmail.com>
2014-01-25 12:46:14 -0600

    config

--- config.js
+++ config.js
@@ -6,11 +6,4 @@ exports.MYSQLdbname='Log';
 exports.MYSQLhost='';
 exports.MYSQLpassword='root';
 exports.MYSQLport='8889';
-/*
-//prod
-exports.MYSQLusername='root';
-exports.MYSQLdbname='mockJSON';
-exports.MYSQLhost='';
-exports.MYSQLpassword='hellcat';
-exports.MYSQLport='3306';
-*/
+
